00332144668475b83ab935768abb27c3b7329711
Sam <SamWheating@yahoo.ca>
2019-01-27 02:23:16 -0800

    Initial dist subtree commit

---
+++ precache-manifest.d599a37cf8340b550d2f8c0e5caf8ef7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<reqcheck>/robots.txt"
+  },
+  {
+    "revision": "9919d3e25842e17c6977",
+    "url": "/<reqcheck>/js/chunk-vendors.9510c993.js"
+  },
+  {
+    "revision": "f72d629ccdb26e12e20a",
+    "url": "/<reqcheck>/js/app.14bcd266.js"
+  },
+  {
+    "revision": "4833023009d8d099b89d",
+    "url": "/<reqcheck>/js/about.127780a8.js"
+  },
+  {
+    "revision": "72b58186c108e4518d70b9fc9d1b0ca7",
+    "url": "/<reqcheck>/index.html"
+  },
+  {
+    "revision": "b7f80d369ebd692a13d347f04d985bcc",
+    "url": "/<reqcheck>/img/req2.b7f80d36.png"
+  },
+  {
+    "revision": "f72d629ccdb26e12e20a",
+    "url": "/<reqcheck>/css/app.d8c43768.css"
+  }
+];
